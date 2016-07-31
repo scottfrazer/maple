405ee089f58cf88e270ecff57e90a949d85de468
Scott Frazer <scott.d.frazer@gmail.com>
2016-07-31 16:30:37 -0400

    contexts

--- context.go
+++ context.go
@@ -3,33 +3,63 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/context"
+	"sync"
 	"time"
 )
 
-func f(id string, ctx context.Context) {
-	childCtx, cancel := context.WithTimeout(ctx, time.Second*2)
+var c = make(chan int)
+
+func g(id string, ctx context.Context, wg *sync.WaitGroup) {
 	defer func() {
-		cancel()
-		fmt.Printf("f(%s) exit\n", id)
+		wg.Done()
+		fmt.Printf("%s exit\n", id)
 	}()
 
-	if id == "A" {
-		go f("B", childCtx)
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%s cancel: %s\n", id, ctx.Err())
+	case <-c:
+		fmt.Printf("shouldn't happen")
+	}
+}
+
+func f(id string, ctx context.Context, wg *sync.WaitGroup) {
+	//childCtx, cancel := context.WithTimeout(ctx, time.Second*2)
+	childCtx, cancel := context.WithCancel(ctx)
+	var childWg sync.WaitGroup
+
+	defer func() {
+		cancel()
+		childWg.Wait()
+		wg.Done()
+		fmt.Printf("%s exit\n", id)
+	}()
+
+	for i := 0; i < 2; i++ {
+		childWg.Add(1)
+		go g(fmt.Sprintf("%s-g%d", id, i), childCtx, &childWg)
 	}
 
 	select {
 	case <-ctx.Done():
-		fmt.Printf("f(%s) cancel: %s\n", id, ctx.Err())
-	case <-time.After(time.Second * 5):
-		fmt.Printf("f(%s) timeout\n", id)
+		fmt.Printf("%s cancel: %s\n", id, ctx.Err())
+	case <-c:
+		fmt.Printf("shouldn't happen")
 	}
 }
 
-func main2() {
+func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	go f("A", ctx)
+	var wg sync.WaitGroup
 
-	fmt.Println(ctx, cancel)
-	time.Sleep(time.Second * 15)
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go f(fmt.Sprintf("f%d", i), ctx, &wg)
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("cancel()")
+	cancel()
+	wg.Wait()
 }
